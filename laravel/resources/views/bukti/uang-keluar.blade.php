7128ce17421cf80efbc701c6cd7a3c943d8af7c1
ysb <yuli.setyo@gmail.com>
2019-12-24 21:29:04 +0700

    perbaikan bukti transaksi

--- laravel/resources/views/bukti/uang-keluar.blade.php
+++ laravel/resources/views/bukti/uang-keluar.blade.php
@@ -12,17 +12,17 @@
 	<tbody>
 		<tr>
 			<td class="wd10 pl3 vt">Nomor</td>
-			<td class="wd2 pl3 vt">:</td>
+			<td class="wd2 vt">:</td>
 			<td class="pl3 vt">&nbsp;</td>
 		</tr>
 		<tr>
 			<td class="pl3 vt">Tahun</td>
-			<td class="pl3 vt">:</td>
+			<td class=" t">:</td>
 			<td class="pl3 vt">{{$tahun}}</td>
 		</tr>
 		<tr>
 			<td class="pl3 vt">No. Bukti Keluar</td>
-			<td class="pl3 vt">:</td>
+			<td class="vt">:</td>
 			<td class="pl3 vt">&nbsp;</td>
 		</tr>
 	</tbody>
@@ -35,36 +35,36 @@
 		</tr>
 	</thead>
 </table>
-
+<br>
 <!--table content-->
-<table width="100%" border="1" cellspacing="0" cellpadding="3" style="border: 1px solid #000;font-size:12px;">
+<table width="100%" border="1" cellspacing="0" cellpadding="3" style="border:1px solid #000;font-size:11px;">
 	<tbody>
 		<tr>
 			<td class="wd15 pl3 vt">Tahun Anggaran</td>
-			<td class="wd3 pl3 vt">:</td>
+			<td class="wd5 vt">:</td>
 			<td class="wd20 pl3 vt">{{$tahun}}</td>
-			<td colspan="3" class="bl pl3">&nbsp;</td>
+			<td colspan="4" class="bl pl3">&nbsp;</td>
 		</tr>
 		<tr>
 			<td class="pl3 vt">Otorisasi Pengeluaran</td>
-			<td class="pl3 vt">:</td>
+			<td class="vt">:</td>
 			<td class="pl3 vt">...........</td>
-			<td colspan="3" class="bl pl3">&nbsp;</td>
+			<td colspan="4" class="bl pl3">&nbsp;</td>
 		</tr>
 		<tr>
 			<td colspan="3">&nbsp;</td>
-			<td rowspan="2" class="bl pl3 vt">Kepada</td>
-			<td rowspan="2" class="pl3 vt">:</td>
-			<td rowspan="2" colspan="2" class="pl3 vt">........................................................................................</td>
+			<td rowspan="2" class="wd15 bl pl3 vt">Kepada</td>
+			<td rowspan="2" class="vt">:</td>
+			<td rowspan="2" colspan="2" class="pl3 vt">..............................................................................</td>
 		</tr>
 		<tr>
 			<td colspan="3">&nbsp;</td>
 		</tr>
 		<tr>
 			<td colspan="3" class="pl3">Kode Mata Anggaran Pengeluaran :</td>
-			<td rowspan="3" class="bl wd15 pl3 vt">Uang Sejumlah</td>
-			<td rowspan="3" class="wd2 pl3 vt">:</td>
-			<td rowspan="3" colspan="2" class="pl3 vt">........................................................................................</td>
+			<td rowspan="3" class="bl pl3 vt">Uang Sejumlah</td>
+			<td rowspan="3" class="wd2 vt">:</td>
+			<td rowspan="3" colspan="2" class="pl3 vt">..............................................................................</td>
 		</tr>
 		<tr>
 			<td colspan="3" class="pl3">Beban Anggaran Belanja</td>
@@ -74,62 +74,62 @@
 		</tr>
 		<tr>
 			<td colspan="3">&nbsp;</td>
-			<td colspan="3" class="bl">&nbsp;</td>
+			<td colspan="4" class="bl">&nbsp;</td>
 		</tr>
 		<tr>
 			<td class="pl3">....................</td>
 			<td class="pl3">Rp</td>
 			<td class="pl3 ar">....................</td>
 			<td rowspan="4" class="bl pl3 vt">Untuk</td>
-			<td rowspan="4" class="wd2 pl3 vt">:</td>
+			<td rowspan="4" class="pl3 vt">:</td>
 			<td rowspan="4" colspan="2" class="pl3 vt">........................................................................................</td>
 		</tr>
 		<tr>
 			<td class="pl3">....................</td>
-			<td class="pl3">Rp</td>
+			<td class="vt">Rp</td>
 			<td class="pl3 ar">....................</td>
 		</tr>
 		<tr>
 			<td class="pl3">....................</td>
-			<td class="pl3">Rp</td>
+			<td class="vt">Rp</td>
 			<td class="pl3 ar">....................</td>
 		</tr>
 		<tr>
 			<td class="pl3">....................</td>
-			<td class="pl3">Rp</td>
+			<td class="vt">Rp</td>
 			<td class="pl3 ar">....................</td>
 		</tr>
 		<tr>
 			<td class="pl3">....................</td>
-			<td class="pl3">Rp</td>
+			<td class="vt">Rp</td>
 			<td class="pl3 ar">....................</td>
 			<td rowspan="4" class="bl pl3 vt">Dasar Pembayaran</td>
-			<td rowspan="4" class="wd2 pl3 vt">:</td>
+			<td rowspan="4" class="pl3 vt">:</td>
 			<td rowspan="4" colspan="2" class="pl3 vt">........................................................................................</td>
 		</tr>
 		<tr>
-			<td class="pl3">....................</td>
-			<td class="pl3">Rp</td>
+			<td class="pl3 vt">....................</td>
+			<td class="vt">Rp</td>
 			<td class="pl3 ar">....................</td>
 		</tr>
 		<tr>
-			<td class="pl3">....................</td>
-			<td class="pl3">Rp</td>
+			<td class="pl3 vt">....................</td>
+			<td class="vt">Rp</td>
 			<td class="pl3 ar">....................</td>
 		</tr>
 		<tr>
-			<td class="pl3">....................</td>
-			<td class="pl3">Rp</td>
+			<td class="pl3 vt">....................</td>
+			<td class="vt">Rp</td>
 			<td class="pl3 ar">....................</td>
 		</tr>
 		<tr>
 			<td colspan="3">&nbsp;</td>
-			<td colspan="3" class="bl">&nbsp;</td>
+			<td colspan="4" class="bl">&nbsp;</td>
 		</tr>
 		<tr>
 			<td colspan="3" rowspan="2" class="pl3 vt">Kode Mata Anggaran Pengembalian :</td>
-			<td colspan="2" class="bl pl3 ac">Kasir/Bendahara:</td>
-			<td class="pl3">&nbsp;</td>
+			<td colspan="3" class="bl pl3 ac">KASIR/BENDAHARA:</td>
+			<td colspan="1" class="pl3">&nbsp;</td>
 		</tr>
 		<tr>
 			<td class="bl pl3">Pada tanggal</td>
@@ -137,80 +137,110 @@
 			<td colspan="2" class="pl3">....................</td>
 		</tr>
 		<tr>
-			<td class="pl3">....................</td>
-			<td class="pl3">Rp</td>
-			<td class="pl3 ar">....................</td>
-			<td class="bl pl3">Cash/Cek</td>
-			<td class="pl3">:</td>
-			<td class="pl3">....................</td>
+			<td class="pl3 vt">....................</td>
+			<td class="vt">Rp</td>
+			<td class="pl3 ar vt">....................</td>
+			<td class="bl pl3 vt">Cash/Cek</td>
+			<td class="vt">:</td>
+			<td colspan="2" class="pl3">....................</td>
 		</tr>
 		<tr>
 			<td class="pl3">....................</td>
 			<td class="pl3">Rp</td>
 			<td class="pl3 ar">....................</td>
 			<td class="bl pl3">Tanda tangan</td>
-			<td class="pl3">:</td>
+			<td class="vt">:</td>
+			<td class="vt">....................</td>
+			<td class="pl3 al">&nbsp;</td>
+		</tr>
+		<tr>
 			<td class="pl3">....................</td>
-			<td class="pl3">Jakarta</td>
+			<td class="pl3">Rp</td>
+			<td class="pl3 ar">....................</td>
+			<td colspan="3" class="bl pl3">&nbsp;</td>
+			<td class="pl3 al">Jakarta</td>
 		</tr>
 		<tr>
 			<td class="pl3">....................</td>
 			<td class="pl3">Rp</td>
 			<td class="pl3 ar"><u>....................</u></td>
-			<td colspan="3" class="bl pl3">&nbsp;</td>
+			<td colspan="3" class="bl pl3 ac"><u>PENERIMA UANG</u></td>
+			<td class="pl3 al">PD PEMBANGUNAN SARANA JAYA</td>
 		</tr>
 		<tr>
 			<td colspan="3" class="pl3">&nbsp;</td>
-			<td colspan="3" class="bl pl3">&nbsp;</td>
+			<td class="bl pl3 vt">	Nama</td>
+			<td class="vt">:</td>
+			<td class="pl3 vt">&nbsp;</td>
+			<td rowspan="4" class="pl3 al vt">DIREKTUR UTAMA/ADMINISTRASI DAN KEUANGAN</td>
 		</tr>
 		<tr>
-			<td class="pl3 vt">Jumlah yang harus dibayar</td>
-			<td class="pl3 vt">Rp</td>
-			<td class="pl3 ar vt">....................</td>
-			<td colspan="3" class="bl pl3">&nbsp;</td>
+			<td rowspan="2" class="pl3 vt">Jumlah yang harus dibayar</td>
+			<td rowspan="2" class="pl3 vt">Rp</td>
+			<td rowspan="2" class="pl3 ar vt">....................</td>
+			<td class="bl pl3 vt">KTP/SIM No.</td>
+			<td class="vt">:</td>
+			<td class="pl3 vt">&nbsp;</td>
+
 		</tr>
 		<tr>
-			<td colspan="3" class="pl3 bb">&nbsp;</td>
-			<td colspan="3" class="bl bb pl3">&nbsp;</td>
+			<td class="bl pl3 vt">Alamat</td>
+			<td class="vt">:</td>
+			<td class="pl3">&nbsp;</td>
 		</tr>
 		<tr>
 			<td colspan="3" class="pl3">&nbsp;</td>
-			<td colspan="3" class="bl pl3">&nbsp;</td>
+			<td class="bl pl3">Tanda tangan</td>
+			<td class="vt">:</td>
+			<td class="pl3">&nbsp;</td>
 		</tr>
 		<tr>
 			<td colspan="3" class="pl3">&nbsp;</td>
-			<td colspan="2" class="bl pl3">&nbsp;</td>
-			<td class="pl3">Jakarta</td>
-		</tr>
-		<tr>
-			<td colspan="3" class="pl3 ac">&nbsp;</td>
-			<td colspan="2" class="bl pl3">&nbsp;</td>
-			<td class="pl3">Senior Manajer Divisi Keuangan & Akuntansi</td>
-		</tr>
-		<tr>
-			<td colspan="3" style="padding:2em;">&nbsp;</td>
 			<td colspan="2" class="bl">&nbsp;</td>
+			<td class="">&nbsp;</td>
+			<td class="">(...................................)</td>
+		</tr>
+		<tr>
+			<td colspan="3" class="pl3">&nbsp;</td>
+			<td colspan="2" class="bl pl3">&nbsp;</td>
+			<td class="pl3">&nbsp;</td>
+			<td class="pl3">NIP/NRK</td>
+		</tr>
+		<tr>
+			<td colspan="3" class="bb pl3">&nbsp;</td>
+			<td colspan="2" class="bl bb pl3">&nbsp;</td>
+			<td class="bb pl3">&nbsp;</td>
+			<td class="bb pl3">&nbsp;</td>
+		</tr>
+		<tr>
+			<td colspan="3" class="pl3">&nbsp;</td>
+			<td colspan="2" class="bl pl3">&nbsp;</td>
+			<td class="pl3">&nbsp;</td>
 			<td class="pl3">&nbsp;</td>
 		</tr>
 		<tr>
 			<td colspan="3" class="pl3 ac">&nbsp;</td>
 			<td colspan="2" class="bl pl3">&nbsp;</td>
-			<td class="pl3">(........................................)</td>
+			<td class="pl3">Senior Manajer Divisi Keuangan & Akuntansi</td>
+			<td class="pl3">&nbsp;</td>
+		</tr>
+		<tr>
+			<td colspan="3" style="padding:2em; bt">&nbsp;</td>
+			<td colspan="2" class="bl">Telah diperiksa dan dibukukan dalam Buku Kas/Bank dengan nomor: <br>...................................</td>
+			<td class="pl3">&nbsp;</td>
+			<td class="pl3">&nbsp;</td>
+		</tr>
+		<tr>
+			<td colspan="3" class="pl3 ac">&nbsp;</td>
+			<td colspan="2" class="bl pl3">&nbsp;</td>
+			<td class="pl3">(...................................)</td>
+			<td class="pl3">&nbsp;</td>
 		</tr>
 		
 		<tr>
 			<td colspan="3" class="pl3 ac">&nbsp;</td>
-			<td colspan="2" class="bl pl3">Telah diperiksa dan dibukukan dalam Buku Kas/Bank dengan nomor:</td>
-			<td class="pl3 ac">&nbsp;</td>
-		</tr>
-		<tr>
-			<td colspan="3" class="pl3 ac">&nbsp;</td>
-			<td colspan="2" class="bl pl3">........................................</td>
-			<td class="pl3 ac">&nbsp;</td>
-		</tr>
-		<tr>
-			<td colspan="3" class="pl3">&nbsp;</td>
 			<td colspan="2" class="bl pl3">&nbsp;</td>
+			<td class="pl3 ac">&nbsp;</td>
 			<td class="pl3">&nbsp;</td>
 		</tr>
 	</tbody>
