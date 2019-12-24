2b7bad46e162c65127f6e3d0b9f2c08b0d766669
Eko Firmansyah <ekox69@gmail.com>
2019-12-24 10:19:45 +0700

    perbaikan dashboard

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -21,58 +21,30 @@ class HomeController extends Controller {
 					round(d.nilai4/1000000) as nilai4
 			from(
 				select  count(a.id) as jml1,
-						sum(b.nilai) as nilai1
+						sum(a.nilai) as nilai1
 				from d_trans a
-				left outer join(
-					select  id_trans,
-							sum(nilai) as nilai
-					from d_trans_akun
-					where kddk='D'
-					group by id_trans
-				) b on(a.id=b.id_trans)
 				left outer join t_alur c on(a.id_alur=c.id)
 				where a.thang='".session('tahun')."' and c.menu=1
 			) a,
 			(
 				select  count(a.id) as jml2,
-						sum(b.nilai) as nilai2
+						sum(a.nilai) as nilai2
 				from d_trans a
-				left outer join(
-					select  id_trans,
-							sum(nilai) as nilai
-					from d_trans_akun
-					where kddk='D'
-					group by id_trans
-				) b on(a.id=b.id_trans)
 				left outer join t_alur c on(a.id_alur=c.id)
 				where a.thang='".session('tahun')."' and c.menu=2
 			) b,
 			(
 				select  count(a.id) as jml3,
-						sum(b.nilai) as nilai3
+						sum(a.nilai) as nilai3
 				from d_trans a
-				left outer join(
-					select  id_trans,
-							sum(nilai) as nilai
-					from d_trans_akun
-					where kddk='D'
-					group by id_trans
-				) b on(a.id=b.id_trans)
 				left outer join t_alur c on(a.id_alur=c.id)
 				where a.thang='".session('tahun')."' and c.menu=3
 			) c,
 
 			(
 				select  count(a.id) as jml4,
-						sum(b.nilai) as nilai4
+						sum(a.nilai) as nilai4
 				from d_trans a
-				left outer join(
-					select  id_trans,
-							sum(nilai) as nilai
-					from d_trans_akun
-					where kddk='D'
-					group by id_trans
-				) b on(a.id=b.id_trans)
 				left outer join t_alur c on(a.id_alur=c.id)
 				where a.thang='".session('tahun')."' and c.menu=4
 			) d
