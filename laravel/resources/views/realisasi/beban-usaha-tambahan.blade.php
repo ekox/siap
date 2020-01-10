bf6a1672466d1c60842b657bec0d726c1e3ba4ec
yuli setyo budi <yuli.setyo@gmail.com>
2020-01-10 11:48:03 +0700

    penambahan view untk beban usaha tambahan

---
+++ laravel/resources/views/realisasi/beban-usaha-tambahan.blade.php
@@ -0,0 +1,77 @@
+@include('css')
+
+<table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:0px solid #000;">
+	<thead>
+		<tr>
+			<th colspan="9" class="">
+				<h3>RINCIAN REALISASI PENDAPATAN TAHUN 20XX</h3>
+				<br>
+				(Disajikan dalam Jutaan Rupiah)
+			</th>
+		</tr>
+		<tr>
+			<th colspan="9" class="">&nbsp;</th>
+		</tr>
+		<tr>
+			<th class="bl bt plr3">Uraian</th>
+			<th class="wd12 bl bt plr3">RKAP 20XX</th>
+			<th class="wd12 bl bt plr3">Rencana TW.II 20XX</th>
+			<th class="wd12 bl bt plr3">Realisasi TW.II 20XX</th>
+			<th class="wd5 bl bt plr3">%</th>
+			<th class="wd12 bl bt plr3">Rencana s.d TW.II 20XX</th>
+			<th class="wd12 bl bt plr3">Realisasi s.d TW.II 20XX</th>
+			<th class="wd5 bl bt plr3">%</th>
+			<th class="wd5 bl bt br plr3">%</th>
+		</tr>
+		<tr>
+			<td class="bl bt ac fs10">1</td>
+			<td class="bl bt ac fs10">2</td>
+			<td class="bl bt ac fs10">3</td>
+			<td class="bl bt ac fs10">4</td>
+			<td class="bl bt ac fs10">5</td>
+			<td class="bl bt ac fs10">6</td>
+			<td class="bl bt ac fs10">7</td>
+			<td class="bl bt ac fs10">8</td>
+			<td class="bl bt br ac fs10">9</td>
+		</tr>
+	</thead>
+</table>
+<table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:0px solid #000;font-size:12px;">
+	<tbody>
+		<tr>
+			<td class="bl bt al plr3">BEBAN USAHA</td>
+			<td class="wd12 bl bt al plr3">&nbsp;</td>
+			<td class="wd12 bl bt al plr3">&nbsp;</td>
+			<td class="wd12 bl bt al plr3">&nbsp;</td>
+			<td class="wd5 bl bt al plr3">&nbsp;</td>
+			<td class="wd12 bl bt al plr3">&nbsp;</td>
+			<td class="wd12 bl bt al plr3">&nbsp;</td>
+			<td class="wd5 bl bt al plr3">&nbsp;</td>
+			<td class="wd5 bl bt br al plr3">&nbsp;</td>
+		</tr>
+		@foreach($rows1 as $row)
+		<tr>
+			<td class="bl al plr3">{{ $row['uraian'] }}</td>
+			<td class="bl ar plr3">{{ $row['rkap'] }}</td>
+			<td class="bl ar plr3">{{ $row['rctw'] }}</td>
+			<td class="bl ar plr3">{{ $row['rltw'] }}</td>
+			<td class="bl ar plr3">{{ $row['psn1'] }}</td>
+			<td class="bl ar plr3">{{ $row['rcsdtw'] }}</td>
+			<td class="bl ar plr3">{{ $row['rlsdtw'] }}</td>
+			<td class="bl ar plr3">{{ $row['psn2'] }}</td>
+			<td class="bl br ar plr3">{{ $row['psn3'] }}</td>
+		</tr>
+		@endforeach
+		<tr>
+			<td class="bl bt ac plr3">Jumlah</td>
+			<td class="bl bt ar plr3">0</td>
+			<td class="bl bt ar plr3">0</td>
+			<td class="bl bt ar plr3">0</td>
+			<td class="bl bt ar plr3">0</td>
+			<td class="bl bt ar plr3">0</td>
+			<td class="bl bt ar plr3">0</td>
+			<td class="bl bt ar plr3">0</td>
+			<td class="bl bt ar plr3">0</td>
+		</tr>
+	</tbody>
+</table>
